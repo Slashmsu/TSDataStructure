ae94c353097251e06c0dfdc216f1700c34910b4c
codefactor-io <support@codefactor.io>
2019-09-24 21:07:48 +0000

    [CodeFactor] Apply fixes

--- Tests/Dictionary.spec.ts
+++ Tests/Dictionary.spec.ts
@@ -13,7 +13,7 @@ describe('Dictionary', () => {
         expect(dictionary.isEmpty()).toEqual(true);
         expect(dictionary.size()).toEqual(0);
 
-    }); 
+    });
     
     test('isEmpty()', () => {
 
@@ -21,37 +21,37 @@ describe('Dictionary', () => {
         dictionary.put("key", 1);
         expect(dictionary.isEmpty()).toEqual(false);
 
-    }); 
+    });
 
     test('size()', () => {
 
         expect(dictionary.size()).toEqual(0);
         dictionary.put("key", 1);
-        expect(dictionary.size()).toEqual(1); 
+        expect(dictionary.size()).toEqual(1);
 
-    }); 
+    });
 
     test('put()', () => {
 
         dictionary.put("key", 1);
-        expect(dictionary.get("key")).toEqual(1); 
+        expect(dictionary.get("key")).toEqual(1);
 
-    }); 
+    });
 
     test('get()', () => {
 
         dictionary.put("key", 1);
-        expect(dictionary.get("key")).toEqual(1); 
+        expect(dictionary.get("key")).toEqual(1);
 
-    }); 
+    });
 
     test('remove()', () => {
 
         dictionary.put("key", 1);
         dictionary.remove("key");
-        expect(dictionary.size()).toEqual(0); 
+        expect(dictionary.size()).toEqual(0);
 
-    }); 
+    });
 
     test('clear()', () => {
 
@@ -59,46 +59,46 @@ describe('Dictionary', () => {
         dictionary.put("key2", 2);
         dictionary.put("key3", 3);
         dictionary.clear();
-        expect(dictionary.size()).toEqual(0); 
+        expect(dictionary.size()).toEqual(0);
 
-    });  
+    });
 
     test('keys()', () => {
 
         dictionary.put("key1", 1);
         dictionary.put("key2", 2);
         dictionary.put("key3", 3);
-        expect(dictionary.keys()).toEqual(["key1", "key2", "key3"]); 
+        expect(dictionary.keys()).toEqual(["key1", "key2", "key3"]);
 
-    }); 
+    });
 
     test('toArray()', () => {
 
         dictionary.put("key1", 1);
         dictionary.put("key2", 2);
         dictionary.put("key3", 3);
-        expect(dictionary.toArray()).toEqual([1, 2, 3]); 
+        expect(dictionary.toArray()).toEqual([1, 2, 3]);
 
-    }); 
+    });
 
     test('setValue()', () => {
 
         dictionary.put("key1", 1);
         dictionary.put("key2", 2);
         dictionary.put("key3", 3);
-        expect(dictionary.setValue("key2", 7)).toEqual(true); 
-        expect(dictionary.setValue("key5", 9)).toEqual(false); 
+        expect(dictionary.setValue("key2", 7)).toEqual(true);
+        expect(dictionary.setValue("key5", 9)).toEqual(false);
 
-    }); 
+    });
 
     test('containsKey()', () => {
 
         dictionary.put("key1", 1);
         dictionary.put("key2", 2);
         dictionary.put("key3", 3);
-        expect(dictionary.containsKey("key2")).toEqual(true); 
-        expect(dictionary.containsKey("key5")).toEqual(false); 
+        expect(dictionary.containsKey("key2")).toEqual(true);
+        expect(dictionary.containsKey("key5")).toEqual(false);
 
-    }); 
+    });
     
 });
