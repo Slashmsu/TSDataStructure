a5d9af6d87dad21730e422ba5c24085ca1e065f2
SlashMSU <slashmsu@gmail.com>
2019-10-03 18:00:13 +0200

    refactoring and cleaning

--- 1jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "roots": [
-      "<rootDir>/tests"
-    ],
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-  }
--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  roots: [
-    '<rootDir>/tests',
-  ],
-}
